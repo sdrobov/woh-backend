7602a24850980bae312d1099c7de8d8ea9f1ce71
Sergey Drobov <serch.m@gmail.com>
2019-07-09 20:28:16 +0300

    Teaser list and byId

--- src/main/java/ru/woh/api/controllers/admin/AdminTeaserController.java
+++ src/main/java/ru/woh/api/controllers/admin/AdminTeaserController.java
@@ -1,9 +1,12 @@
 package ru.woh.api.controllers.admin;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.HttpClientErrorException;
 import ru.woh.api.models.Post;
@@ -11,9 +14,13 @@ import ru.woh.api.models.Role;
 import ru.woh.api.models.Teaser;
 import ru.woh.api.models.repositories.PostRepository;
 import ru.woh.api.models.repositories.TeaserRepository;
+import ru.woh.api.views.admin.TeaserListView;
 import ru.woh.api.views.admin.TeaserView;
 
 import javax.annotation.security.RolesAllowed;
+import javax.websocket.server.PathParam;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class AdminTeaserController {
@@ -29,8 +36,8 @@ public class AdminTeaserController {
         this.postRepository = postRepository;
     }
 
-    @PostMapping({ "/teasers", "/teasers/" })
-    @RolesAllowed({ Role.ROLE_MODER, Role.ROLE_ADMIN })
+    @PostMapping({"/teasers", "/teasers/"})
+    @RolesAllowed({Role.ROLE_MODER, Role.ROLE_ADMIN})
     public TeaserView add(TeaserView teaserView) {
         Post post = this.postRepository.findById(teaserView.getPost().getId())
             .orElseThrow(() -> new HttpClientErrorException(
@@ -39,21 +46,21 @@ public class AdminTeaserController {
         Teaser teaser = new Teaser();
         teaser.setFrom(teaserView.getFrom());
         teaser.setTo(teaserView.getTo());
-        teaser.setIsTeaser(teaserView.getTeaser() ? (short)1 : 0);
+        teaser.setIsTeaser(teaserView.getTeaser() ? (short) 1 : 0);
         teaser.setPost(post);
         teaser.autoCreatePk();
 
         return this.teaserRepository.save(teaser).adminView();
     }
 
-    @PostMapping({ "/teasers/delete", "/teasers/delete/" })
-    @RolesAllowed({ Role.ROLE_MODER, Role.ROLE_ADMIN })
+    @PostMapping({"/teasers/delete", "/teasers/delete/"})
+    @RolesAllowed({Role.ROLE_MODER, Role.ROLE_ADMIN})
     public ResponseEntity delete(TeaserView teaserView) {
         Teaser.TeaserPK teaserPK = new Teaser.TeaserPK(
             teaserView.getFrom(),
             teaserView.getTo(),
             teaserView.getPost().getId(),
-            teaserView.getTeaser() ? (short)1 : 0
+            teaserView.getTeaser() ? (short) 1 : 0
         );
 
         Teaser teaser = this.teaserRepository.findById(teaserPK)
@@ -62,5 +69,33 @@ public class AdminTeaserController {
         this.teaserRepository.delete(teaser);
 
         return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @GetMapping({"/teasers/all", "/teasers/all/"})
+    @RolesAllowed({Role.ROLE_MODER, Role.ROLE_ADMIN})
+    public TeaserListView all(@RequestParam(value = "page", defaultValue = "0") Integer page) {
+        var result = this.teaserRepository.findAll(PageRequest.of(page, 20));
+
+        var teaserListView = new TeaserListView();
+        teaserListView.setCurrentPage(page);
+        teaserListView.setTeasers(result.get().map(Teaser::adminView).collect(Collectors.toList()));
+        teaserListView.setTotalCount(result.getTotalElements());
+        teaserListView.setTotalPages(result.getTotalPages());
+
+        return teaserListView;
+    }
+
+    @GetMapping({"/teasers/{id:[0-9]*}", "/teasers/{id:[0-9]*}/"})
+    @RolesAllowed({Role.ROLE_MODER, Role.ROLE_ADMIN})
+    public List<TeaserView> byPostId(@PathParam("id") Long id) {
+        var post = this.postRepository.findById(id)
+            .orElseThrow(() -> new HttpClientErrorException(HttpStatus.NOT_FOUND,
+                String.format("Post #%d not found", id)));
+
+        return this.teaserRepository
+            .findAllByPost(post)
+            .stream()
+            .map(Teaser::adminView)
+            .collect(Collectors.toList());
     }
 }
--- src/main/java/ru/woh/api/models/repositories/TeaserRepository.java
+++ src/main/java/ru/woh/api/models/repositories/TeaserRepository.java
@@ -1,19 +1,21 @@
 package ru.woh.api.models.repositories;
 
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
+import ru.woh.api.models.Post;
 import ru.woh.api.models.Teaser;
-import ru.woh.api.services.DateTimeService;
 
 import java.util.Date;
 import java.util.List;
 
 @Repository
-public interface TeaserRepository extends CrudRepository<Teaser, Teaser.TeaserPK> {
+public interface TeaserRepository extends PagingAndSortingRepository<Teaser, Teaser.TeaserPK> {
     @Query("SELECT t FROM Teaser t WHERE ?1 BETWEEN t.from AND t.to AND t.isTeaser = 1")
     List<Teaser> findActualTeasers(Date now);
 
     @Query("SELECT t FROM Teaser t WHERE ?1 BETWEEN t.from AND t.to AND t.isTeaser = 0")
     List<Teaser> findActualFeatured(Date now);
+
+    List<Teaser> findAllByPost(Post post);
 }
