6b46e646a9e059b7d0a579b405006f99211d887f
Sergey Drobov <serch.m@gmail.com>
2020-02-05 00:42:45 +0300

    sentry & mongo #2

--- src/main/java/ru/woh/api/ApiApplication.java
+++ src/main/java/ru/woh/api/ApiApplication.java
@@ -1,19 +1,8 @@
 package ru.woh.api;
 
-import com.mongodb.MongoClient;
-import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.gridfs.GridFSBucket;
-import com.mongodb.client.gridfs.GridFSBuckets;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.mongodb.MongoDbFactory;
-import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
-import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
-import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
-import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
-import org.springframework.data.mongodb.gridfs.GridFsTemplate;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
@@ -21,12 +10,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ApiApplication {
-    @Value("${spring.data.mongodb.database}")
-    public String mongoDb;
-
-    @Value("${spring.data.mongodb.host}")
-    public String mongoHost;
-
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
     }
@@ -44,27 +27,5 @@ public class ApiApplication {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public GridFSBucket gridFSBucket() {
-        MongoDatabase mongoDatabase = mongoDbFactory().getDb();
-
-        return GridFSBuckets.create(mongoDatabase);
-    }
-
-    @Bean
-    public MongoDbFactory mongoDbFactory() {
-        return new SimpleMongoDbFactory(new MongoClient(this.mongoHost), this.mongoDb);
-    }
-
-    @Bean
-    public MappingMongoConverter mappingMongoConverter() {
-        return new MappingMongoConverter(new DefaultDbRefResolver(mongoDbFactory()), new MongoMappingContext());
-    }
-
-    @Bean
-    public GridFsTemplate gridFsTemplate() {
-        return new GridFsTemplate(mongoDbFactory(), mappingMongoConverter());
     }
 }
